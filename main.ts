dab6a74333dfcf9f44e575402567838c6c287691
Kevin <kwburnett@gmail.com>
2019-10-23 21:26:20 -0500

    initializing and reformatting

--- main.ts
+++ main.ts
@@ -2,55 +2,54 @@ import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
-let win, serve;
+let browserWindow: BrowserWindow, shouldServe: boolean;
 const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
+shouldServe = args.some(val => val === '--serve');
 
 function createWindow() {
-
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
   // Create the browser window.
-  win = new BrowserWindow({
+  browserWindow = new BrowserWindow({
     x: 0,
     y: 0,
     width: size.width,
     height: size.height,
     webPreferences: {
-      nodeIntegration: true,
-    },
+      nodeIntegration: true
+    }
   });
 
-  if (serve) {
+  if (shouldServe) {
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
-    win.loadURL('http://localhost:4200');
+    browserWindow.loadURL('http://localhost:4200');
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    browserWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+    );
   }
 
-  if (serve) {
-    win.webContents.openDevTools();
+  if (shouldServe) {
+    browserWindow.webContents.openDevTools();
   }
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  browserWindow.on('closed', () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    browserWindow = null;
   });
-
 }
 
 try {
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
@@ -68,11 +67,10 @@ try {
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+    if (browserWindow === null) {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
