458176107c9506cf9d12fc83f625930fbc94fc35
Kevin <kwburnett@gmail.com>
2020-01-30 12:27:40 +0100

    updating the menu buttons

--- main.ts
+++ main.ts
@@ -1,10 +1,109 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, Menu, MenuItem, screen } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
-let browserWindow: BrowserWindow, shouldServe: boolean;
+let browserWindow: BrowserWindow, shouldServe: boolean, isMac: boolean;
 const args = process.argv.slice(1);
 shouldServe = args.some(val => val === '--serve');
+isMac = process.platform === 'darwin';
+
+const template: MenuItem[] = [
+  // { role: 'appMenu' }
+  ...(isMac
+    ? [
+        new MenuItem({
+          label: app.getName(),
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        })
+      ]
+    : []),
+  // { role: 'fileMenu' }
+  new MenuItem({
+    label: 'File',
+    submenu: [
+      { label: 'New...', accelerator: 'CmdOrCtrl+N' },
+      { label: 'Open...', accelerator: 'CmdOrCtrl+O' },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  }),
+  // { role: 'editMenu' }
+  new MenuItem({
+    label: 'Edit',
+    submenu: Menu.buildFromTemplate([
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      // ...(isMac
+      //   ? [
+      //       { role: 'pasteAndMatchStyle' },
+      //       { role: 'delete' },
+      //       { role: 'selectAll' },
+      //       { type: 'separator' },
+      //       {
+      //         label: 'Speech',
+      //         submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }]
+      //       }
+      //     ]
+      //   : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }])
+    ])
+  }),
+  // { role: 'viewMenu' }
+  new MenuItem({
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  }),
+  // { role: 'windowMenu' }
+  new MenuItem({
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      // ...(isMac
+      //   ? [
+      //       { type: 'separator' },
+      //       { role: 'front' },
+      //       { type: 'separator' },
+      //       { role: 'window' }
+      //     ]
+      //   : [{ role: 'close' }])
+    ]
+  }),
+  // Menu.buildFromTemplate([{
+  //   role: 'help',
+  //   submenu: [
+  //     {
+  //       label: 'Learn More',
+  //       click: async () => {
+  //         const { shell } = require('electron');
+  //         await shell.openExternal('https://electronjs.org');
+  //       }
+  //     }
+  //   ]
+  // }])
+];
 
 function createWindow() {
   const electronScreen = screen;
@@ -59,7 +158,7 @@ try {
   app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (!isMac) {
       app.quit();
     }
   });
@@ -71,6 +170,9 @@ try {
       createWindow();
     }
   });
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 } catch (e) {
   // Catch Error
   // throw e;
