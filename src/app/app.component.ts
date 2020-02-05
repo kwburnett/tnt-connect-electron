4551f79dbf8495b04f29eb7c2e8060a2ddf56465
Kevin <kwburnett@gmail.com>
2020-02-05 19:34:36 +0100

    adding navigation between two tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
-import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
+import { ElectronService } from './core/services';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    public electronService: ElectronService,
-    private translate: TranslateService
-  ) {
-    translate.setDefaultLang('en');
-    console.log('AppConfig', AppConfig);
+	public activeRoute: string = 'home';
 
-    if (electronService.isElectron) {
-      console.log(process.env);
-      console.log('Mode electron');
-      console.log('Electron ipcRenderer', electronService.ipcRenderer);
-      console.log('NodeJS childProcess', electronService.childProcess);
-    } else {
-      console.log('Mode web');
-    }
-  }
+	constructor(
+		public electronService: ElectronService,
+		private translate: TranslateService,
+		public router: Router
+	) {
+		translate.setDefaultLang('en');
+		console.log('AppConfig', AppConfig);
+
+		if (electronService.isElectron) {
+			console.log(process.env);
+			console.log('Mode electron');
+			console.log('Electron ipcRenderer', electronService.ipcRenderer);
+			console.log('NodeJS childProcess', electronService.childProcess);
+		} else {
+			console.log('Mode web');
+		}
+	}
+
+	public navigate(route: string): void {
+		if (this.activeRoute !== route) {
+			this.router.navigate([route]);
+		}
+		this.activeRoute = route;
+	}
 }
